7362dab84b73eb3d47ea6811483f2e14ba480f50
ErosMZ <151672271+ErosMZ@users.noreply.github.com>
2023-11-28 20:19:31 +0100

    Delete programación modular/unidad5 directory

--- programación modular/unidad5/ejer2.java
+++
@@ -1,9 +0,0 @@
-public class ejer2 {
-    public static void main(String[] args) {
-
-        for(int x = 1; x < 10000; x++ ){
-            System.out.println("-"+ x +"-" + " "+libreria.FNumDAW.esCapicua(x));
-        }
-    }
-}
-
